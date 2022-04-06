ced187ca8728645590a828139fca82df24d6dfc8
Alessandro Contenti <a.contenti@gmail.com>
2022-04-06 20:04:30 +0200

    initial commit from template
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# microservice-payments
+
+[![Docker Image CI](https://github.com/creative-hub-taass/microservice-payments/actions/workflows/docker-image.yml/badge.svg)](https://github.com/creative-hub-taass/microservice-payments/actions/workflows/docker-image.yml)
+
+Microservizio interazioni
+
+## Linux / Mac (bash)
+
+```shell
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build
+```
+
+## Windows (Powershell)
+
+```powershell
+$env:COMPOSE_DOCKER_CLI_BUILD=1; $env:DOCKER_BUILDKIT=1; docker-compose up --build
+```
---
+++ src/main/java/com/creativehub/backend/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.creativehub.backend.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+				.and()
+				.csrf().disable()
+				.headers(HeadersConfigurer::cacheControl);
+	}
+}
