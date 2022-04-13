a77e2966e2d1190d5342a9a3174b1323d931e0bb
ZathSL <93914398+ZathSL@users.noreply.github.com>
2022-04-13 02:16:47 +0200

    Update PaymentsMicroserviceApplication.java

--- src/main/java/com/creativehub/backend/PaymentsMicroserviceApplication.java
+++ src/main/java/com/creativehub/backend/PaymentsMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class PaymentsMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsMicroserviceApplication.class, args);
